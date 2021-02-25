6d75477cea67823963ac3f4a1cc3d5bb7794a430
mina.h <mina.h@amaryllo.us>
2021-02-25 14:10:14 +0800

    <init project>

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1>Fib Calculator</h1>
+                    <h1>Fib Calculator version 2</h1>
                     <p>
                         Edit <code>src/App.js</code> and save to reload.
                     </p>
