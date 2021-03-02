a4d1da302181766679488dd83f9de0759cd8c153
mina.h <mina.h@amaryllo.us>
2021-03-02 10:29:51 +0800

    <modify><skaffold><comment off persistent container>

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1>Fib Calculator version KUBERNETES! updated</h1>
+                    <h1>Fib Calculator version KUBERNETES! local updated</h1>
                     <p>
                         Edit <code>src/App.js</code> and save to reload.
                     </p>
